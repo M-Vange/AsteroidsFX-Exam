acf392cb1446be2497991109d85d084e38275314
M-Vange <mathias@vange.dk>
2026-05-29 20:19:54 +0200

    Written documentation on pre- and post-conditions for provided interfaces using JavaDoc.

--- Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IGamePluginService.java
+++ Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IGamePluginService.java
@@ -3,10 +3,47 @@ package dk.sdu.mmmi.cbse.common.services;
 import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
 
+/**
+ * This interface is used to define lifecycles for the game, ie. Player, Enemy or Asteroids.
+ * The system uses this interface to load and unload entities when the game starts/stops.
+*/
+
 public interface IGamePluginService {
+
+    /**
+     * Initializes and spawns a plugin's entities into the game world.
+     * <p><b>Pre-conditions:</b></p>
+     * <ul>
+     *     <li>{@code gameData} must be initialized and not null. It provides display boundaries.</li>
+     *     <li>{@code world} must be initialized and not null. It provides the entity container.</li>
+     * </ul>
+     * <p><b>Post-conditions:</b></p>
+     * <ul>
+     *     <li>The plugin's core entities are successfully created and added to the {@code world}.</li>
+     * </ul>
+     *
+     * @param gameData the game state and settings
+     * @param world the container housing all active game entities
+     */
 
     // Runs once when the module is loaded (Spawns entities)
     void start(GameData gameData, World world);
+
+    /**
+     * Cleans up and unloads a plugin's entities when the plugin is stopped.
+     * <p><b>Pre-conditions:</b></p>
+     *  <ul>
+     *      <li>{@code gameData} must not be null.</li>
+     *      <li>{@code world} must not be null.</li>
+     *  </ul>
+     *  <p><b>Post-conditions:</b></p>
+     *  <ul>
+     *      <li>All entities from this plugin are removed from the {@code world}.</li>
+     *  </ul>
+     *
+     * @param gameData the game state and settings
+     * @param world the container housing all active game entities
+     */
 
     // Runs once when the module is unloaded (Cleans up entities)
     void stop(GameData gameData, World world);
--- Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IPostEntityProcessingService.java
+++ Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IPostEntityProcessingService.java
@@ -4,10 +4,28 @@ import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
 
 /**
- *
- * @author jcs
+ * This interface defines what happens at the end of a game loop tick.
+ * This is primarily used for detecting collisions, screen-wrapping and cleanup.
  */
+
 public interface IPostEntityProcessingService {
+
+    /**
+     * Handles the final checks for processing of entities in the game world.
+     * <p><b>Pre-conditions:</b></p>
+     * <ul>
+     *     <li>{@code gameData} must not be null.</li>
+     *     <li>{@code world} must not be null. And it must contain any entities that have finished an update.</li>
+     * </ul>
+     * <p><b>Post-conditions:</b></p>
+     * <ul>
+     *     <li>Interactions for entities have been processed, ie. a collision.</li>
+     *     <li>Entities that have been flagged as destroyed by any interaction are removed from the {@code world}.</li>
+     * </ul>
+     *
+     * @param gameData the game state and settings
+     * @param world the container housing all active game entities
+     */
 
     void process(GameData gameData, World world);
 }
