8035888a0cb833d23e9e888fd3d014cea57b88c1
M-Vange <mathias@vange.dk>
2026-05-30 18:15:30 +0200

    Added contract specifications for pre- and post-conditions for IAsteroidSplitter.

--- CommonAsteroids/src/main/java/dk/sdu/mmmi/cbse/common/asteroids/IAsteroidSplitter.java
+++ CommonAsteroids/src/main/java/dk/sdu/mmmi/cbse/common/asteroids/IAsteroidSplitter.java
@@ -4,9 +4,27 @@ import dk.sdu.mmmi.cbse.common.data.Entity;
 import dk.sdu.mmmi.cbse.common.data.World;
 
 /**
- *
- * @author corfixen
+ * This interface provides a contract for splitting a parent asteroid into smaller pieces upon being hit by a bullet.
  */
+
 public interface IAsteroidSplitter {
+
+    /**
+     * Spawns smaller asteroid entities in the game world if the parent asteroid meets size requirements.
+     * <p><b>Pre-conditions:</b></p>
+     * <ul>
+     *     <li>{@code e} must not be null. Must be a valid asteroid entity and must have triggered a collision with a bullet entity.</li>
+     *     <li>{@code w} must not be null. Must represent the active game world.</li>
+     * </ul>
+     * <p><b>Post-conditions:</b></p>
+     * <ul>
+     *     <li>The asteroid parent entity {@code e} is evaluated and asteroid pieces are instantiated.</li>
+     *     <li>The new asteroid pieces are injected into the active game {@code World w}.</li>
+     * </ul>
+     *
+     * @param e the parent asteroid entity being split
+     * @param w the game world where asteroid pieces will be spawned
+     */
+
     void createSplitAsteroid(Entity e, World w);
 }
