22e251c72f3499995322151b258ce035d31b3de9
M-Vange <mathias@vange.dk>
2026-05-30 14:05:09 +0200

    Implemented asteroid splitter.
    Asteroids now split depending on what collides with them. If they are a certain size, they get split into new small asteroids with new values for flight and randomly generated geometry.
    
    Prepared for ships to be destroyed after a certain number of shots (TBI)

--- Asteroids/src/main/java/dk/sdu/mmmi/cbse/asteroid/AsteroidSplitterImpl.java
+++ Asteroids/src/main/java/dk/sdu/mmmi/cbse/asteroid/AsteroidSplitterImpl.java
@@ -1,17 +1,62 @@
 package dk.sdu.mmmi.cbse.asteroid;
 
+import dk.sdu.mmmi.cbse.common.asteroids.Asteroid;
 import dk.sdu.mmmi.cbse.common.asteroids.IAsteroidSplitter;
 import dk.sdu.mmmi.cbse.common.data.Entity;
 import dk.sdu.mmmi.cbse.common.data.World;
+import java.util.Random;
 
-/**
- *
- * @author corfixen
- */
+
 public class AsteroidSplitterImpl implements IAsteroidSplitter {
+
+    private final Random rnd = new Random();
 
     @Override
     public void createSplitAsteroid(Entity e, World world) {
+        Asteroid original = (Asteroid) e;
+
+        // Getting current size of hit asteroid
+        float originalRadius = original.getRadius();
+
+        // Decision maker determining if it should split or be destroyed
+        // If radius >= 12 pixels, it splits
+        if (originalRadius >= 12) {
+            float newRadius = originalRadius / 2;
+
+            // Spawning two smaller asteroid pieces
+            for (int i = 0; i < 2; i++) {
+                // Assigning split pieces new flying values
+                double randomAngle = rnd.nextInt(360);
+                // Smaller pieces get assigned a faster speed than original asteroids
+                double fasterSpeed = original.getSpeed() * 0.8;
+                double randomSpin = 1.0 + rnd.nextDouble() * 2.0;
+
+                Asteroid smallAsteroid = new Asteroid(fasterSpeed,  randomAngle, randomSpin);
+
+                // Position new split pieces where original asteroid was destroyed
+                smallAsteroid.setX(original.getX());
+                smallAsteroid.setY(original.getY());
+                smallAsteroid.setRadius(newRadius);
+
+                // Generating random bumpy shape for split pieces
+                int points = 7;
+                double[] coordinates = new double[points * 2];
+                for (int j = 0; j < points; j++) {
+                    double angle = 2 * Math.PI * j / points;
+                    double bumpyRadius = newRadius + (rnd.nextInt(5) - 2);
+                    coordinates[j * 2] = Math.cos(angle) * bumpyRadius;
+                    coordinates[j * 2 + 1] = Math.sin(angle) * bumpyRadius;
+                }
+                smallAsteroid.setPolygonCoordinates(coordinates);
+
+                // Spawning split asteroid pieces in game world
+                world.addEntity(smallAsteroid);
+            }
+        }
+
+        // Removing original asteroid from game world
+        world.removeEntity(original);
+
     }
 
 }
--- Collision/src/main/java/module-info.java
+++ Collision/src/main/java/module-info.java
@@ -1,6 +1,10 @@
 import dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
 
 module Collision {
-    requires Common;   
+    requires Common;
+    requires CommonAsteroids;
+
+    uses dk.sdu.mmmi.cbse.common.asteroids.IAsteroidSplitter;
+
     provides IPostEntityProcessingService with dk.sdu.mmmi.cbse.collisionsystem.CollisionDetector;
 }
